3fe87300207c72eacdc17817902a4c130998aaac
lecurde <hajji.wadii@yahoo.fr>
2019-02-12 12:55:42 +0100

    cours2 first commit

--- cours2/counterSaved.js
+++ cours2/counterSaved.js
@@ -1,12 +1,15 @@
 let counterApp = angular.module("counterApp", []);
 
-counterApp.controller("CounterSavedCtrl", ["$scope", $scope => {
-  $scope.load = () => $scope.count = localStorage.getItem("count");
+counterApp.controller("CounterSavedCtrl", [
+  "$scope",
+  $scope => {
+    $scope.load = () => ($scope.count = localStorage.getItem("count"));
 
-  $scope.increment = () => {
-    $scope.count++;
-    localStorage.setItem("count", $scope.count)
+    $scope.increment = () => {
+      $scope.count++;
+      localStorage.setItem("count", $scope.count);
+    };
+
+    $scope.load();
   }
-
-  $scope.load()
-}])
+]);
