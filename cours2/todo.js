fde1916021153cc2e10c4c7316d2c934a4777681
lecurde <hajji.wadii@yahoo.fr>
2019-02-19 14:02:56 +0100

    todo list is done

--- cours2/todo.js
+++ cours2/todo.js
@@ -1,3 +1,24 @@
-let todo1 = angular.module("todoApp", []);
+let todoApp = angular.module("todoApp", []);
 
+todoApp.controller("todoList", [
+  "$scope",
+  $scope => {
+    $scope.tasks = ["do the laundry", "make food for the week"];
+    $scope.deletedTasks = []
 
+    $scope.addTask = () => {
+      $scope.tasks.push($scope.newTask);
+      $scope.newTask = "";
+    };
+
+    $scope.deleteTask = index => {
+      $scope.deletedTasks.push($scope.tasks[index])
+      $scope.tasks.splice(index, 1);
+    };
+
+    $scope.restoreTask = index => {
+      $scope.tasks.push($scope.deletedTasks[index])
+      $scope.deletedTasks.splice(index, 1)
+    }
+  }
+]);
