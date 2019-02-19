e676498891180a07af2ad54fb3b9f7c275bf2d09
lecurde <hajji.wadii@yahoo.fr>
2019-02-19 17:27:16 +0100

    todo done

--- cours2/todo.js
+++ cours2/todo.js
@@ -6,22 +6,16 @@ todoApp.controller("todoList", [
     $scope.tasks = sessionStorage.getItem("tasks")
       ? JSON.parse(sessionStorage.getItem("tasks"))
       : [];
-    $scope.deletedTasks = [];
 
     $scope.addTask = () => {
-      $scope.tasks.push($scope.newTask);
+      $scope.tasks.push({ title: $scope.newTask, done: false });
       $scope.newTask = "";
       sessionStorage.setItem("tasks", JSON.stringify($scope.tasks));
     };
 
-    $scope.deleteTask = index => {
-      $scope.deletedTasks.push($scope.tasks[index]);
-      $scope.tasks.splice(index, 1);
-    };
-
-    $scope.restoreTask = index => {
-      $scope.tasks.push($scope.deletedTasks[index]);
-      $scope.deletedTasks.splice(index, 1);
+    $scope.toggleTask = index => {
+      $scope.tasks[index].done = !$scope.tasks[index].done
+      sessionStorage.setItem("tasks", JSON.stringify($scope.tasks));
     };
   }
 ]);
