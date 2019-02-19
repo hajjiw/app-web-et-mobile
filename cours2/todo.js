ed29682a6c6e8f54d98fbda699b3ca1849a2df08
lecurde <hajji.wadii@yahoo.fr>
2019-02-19 15:39:55 +0100

    basic todo done

--- cours2/todo.js
+++ cours2/todo.js
@@ -3,22 +3,25 @@ let todoApp = angular.module("todoApp", []);
 todoApp.controller("todoList", [
   "$scope",
   $scope => {
-    $scope.tasks = ["do the laundry", "make food for the week"];
-    $scope.deletedTasks = []
+    $scope.tasks = sessionStorage.getItem("tasks")
+      ? JSON.parse(sessionStorage.getItem("tasks"))
+      : [];
+    $scope.deletedTasks = [];
 
     $scope.addTask = () => {
       $scope.tasks.push($scope.newTask);
       $scope.newTask = "";
+      sessionStorage.setItem("tasks", JSON.stringify($scope.tasks));
     };
 
     $scope.deleteTask = index => {
-      $scope.deletedTasks.push($scope.tasks[index])
+      $scope.deletedTasks.push($scope.tasks[index]);
       $scope.tasks.splice(index, 1);
     };
 
     $scope.restoreTask = index => {
-      $scope.tasks.push($scope.deletedTasks[index])
-      $scope.deletedTasks.splice(index, 1)
-    }
+      $scope.tasks.push($scope.deletedTasks[index]);
+      $scope.deletedTasks.splice(index, 1);
+    };
   }
 ]);
