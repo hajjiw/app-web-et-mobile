31a2633f95d4adb745d90ecdf5aa26e946ab88f5
hwadii <wadii.hajji@etu.univ-amu.fr>
2019-04-01 01:39:22 +0200

    add login routes

--- cours3/routes/routes.js
+++ cours3/routes/routes.js
@@ -1,8 +1,10 @@
-const Todo = require('../models/models').Todo;
+const Todo = require('../models/todo');
+const User = require('../models/user');
 
 const list = async (req, res) => {
   try {
-    return res.json(await Todo.find());
+    if (req.session.user) return res.json(await Todo.find());
+    else return res.redirect('/login');
   } catch (error) {
     res.send(error);
   }
@@ -63,4 +65,57 @@ const update_todo = async (req, res) => {
   }
 };
 
-module.exports = { list, add, update, remove, update_todo, get_todo };
+const login = async (req, res) => {
+  try {
+    const result = await User.authenticate(
+      req.body.username,
+      req.body.password
+    );
+    req.session.user = result;
+    return res.send(result);
+  } catch (error) {
+    return res.send(error);
+  }
+};
+
+const signin = async (req, res) => {
+  try {
+    await User.create({
+      username: req.body.username,
+      password: req.body.password
+    });
+    return res.redirect('/login');
+  } catch (error) {
+    return res.send(error);
+  }
+};
+
+const logout = (req, res) => {
+  try {
+    req.session.destroy();
+    return res.redirect('/login');
+  } catch (error) {
+    return res.send(error);
+  }
+};
+
+const get_current_user = (req, res) => {
+  try {
+    return res.send(req.session.user.username);
+  } catch (error) {
+    return res.send(error);
+  }
+};
+
+module.exports = {
+  list,
+  add,
+  update,
+  remove,
+  update_todo,
+  get_todo,
+  login,
+  logout,
+  signin,
+  get_current_user
+};
--- cours3/server.js
+++ cours3/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const app = express();
@@ -11,19 +12,45 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'my secret'
+  })
+);
 
 // on connecte
 mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true });
 
-app.get('/api/list', routes.list);
-app.get('/login', (req, res) => {
-  res.sendFile('/public/login.html', { root: __dirname });
+app.get('/', (req, res) => {
+  console.log(req.session.user);
+  if (!req.session.user) res.redirect('/login');
 });
-app.get('/api/list/todo/:list_id', routes.get_todo);
-app.post('/api/list', routes.add);
-app.put('/api/list/:list_id', routes.update);
-app.put('/api/list/todo/:list_id', routes.update_todo);
-app.delete('/api/list/:list_id', routes.remove);
+app
+  .route('/api/list')
+  .get(routes.list)
+  .post(routes.add);
+
+app
+  .route('/api/list/:list_id')
+  .put(routes.update)
+  .delete(routes.remove);
+app
+  .route('/api/list/todo/:list_id')
+  .get(routes.get_todo)
+  .put(routes.update_todo);
+
+app
+  .route('/login')
+  .get((req, res) => {
+    res.sendFile('/public/login.html', { root: __dirname });
+  })
+  .post(routes.login);
+
+app.get('/api/list/user', routes.get_current_user);
+app.get('/logout', routes.logout);
+app.post('/signin', routes.signin);
 
 app.listen(3001, () => {
   console.log('express started on 3001');
